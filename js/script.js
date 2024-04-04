dcfd13bdc5cb02dd1411d967369126896979d2fd
khanloo <m.nadirkhanloo1380@gmail.com>
2024-04-04 15:27:51 +0330

    scroll header

--- js/script.js
+++ js/script.js
@@ -5,13 +5,24 @@ const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 
+const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 
-const fixNav = document.querySelector("#section-1").getBoundingClientRect();
-window.addEventListener("scroll", function () {
-  if (window.scrollY > fixNav.top) {
+const navY = nav.getBoundingClientRect().height;
+
+const fixedNav = function (e) {
+  const [entry] = e;
+  if (!entry.isIntersecting) {
     nav.classList.add("fixed-top");
   } else {
     nav.classList.remove("fixed-top");
   }
+};
+
+const headerObserver = new IntersectionObserver(fixedNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navY}px`,
 });
+
+headerObserver.observe(header);
