25a6eb1ca4585238b4c7fe85ae43dbec0ba1e494
khanloo <m.nadirkhanloo1380@gmail.com>
2024-04-04 15:06:57 +0330

    add scroll nav

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+const nav = document.querySelector("nav");
+
+const fixNav = document.querySelector("#section-1").getBoundingClientRect();
+window.addEventListener("scroll", function () {
+  if (window.scrollY > fixNav.top) {
+    nav.classList.add("fixed-top");
+  } else {
+    nav.classList.remove("fixed-top");
+  }
+});
