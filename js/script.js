a67f220748049fe562c5bf801a6ebbf4f99985e0
khanloo <m.nadirkhanloo1380@gmail.com>
2024-10-06 10:05:04 +0330

    style code

--- js/script.js
+++ js/script.js
@@ -11,28 +11,28 @@ const nav = document.querySelector("nav");
 const navY = nav.getBoundingClientRect().height;
 
 const fixedNav = function (e) {
-    const [entry] = e;
-    if (!entry.isIntersecting) {
-        nav.classList.add("fixed-top");
-    } else {
-        nav.classList.remove("fixed-top");
-    }
+  const [entry] = e;
+  if (!entry.isIntersecting) {
+    nav.classList.add("fixed-top");
+  } else {
+    nav.classList.remove("fixed-top");
+  }
 };
 
 const headerObserver = new IntersectionObserver(fixedNav, {
-    root: null,
-    threshold: 0,
-    rootMargin: `-${navY}px`,
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navY}px`,
 });
 
 headerObserver.observe(header);
 
 const allsection = document.querySelectorAll("section");
-const showsection = function (e,observer) {
+const showsection = function (e, observer) {
   const [entry] = e;
-  if(!entry.isIntersecting) return
+  if (!entry.isIntersecting) return;
   entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target)
+  observer.unobserve(entry.target);
 };
 const sectionObserver = new IntersectionObserver(showsection, {
   root: null,
